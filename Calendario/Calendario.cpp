39bfa64e28618e03e78f22561206f8637f31fa57
rob1913r <148152111+rob1913r@users.noreply.github.com>
2023-11-24 16:05:04 -0300

    Update Calendario.cpp

--- Calendario/Calendario.cpp
+++ Calendario/Calendario.cpp
@@ -1,21 +1,28 @@
 #include "Calendario.hpp"
 
-#include <iostream>
-#include <map>
-#include <vector>
-
 void Calendario::inserir(std::string key, std::vector<std::string> value) {
 
     _mapaHorario.insert(std::pair<std::string, std::vector<std::string>>(key, value));
 }
 
+unsigned Calendario::size_mapaHorario() {
+
+    return _mapaHorario.size();
+}
+
 void Calendario::inserir(std::string key) {
 
     _agenda.insert(std::pair<std::string, std::map<std::string, std::vector<std::string>>>(key, _mapaHorario));
+    _mapaHorario.clear();
+}
+
+unsigned Calendario::size_agenda() {
+
+    return _agenda.size();
 }
 
 void Calendario::imprimirCalendario(std::string dia) {
-    
+
     std::map<std::string, std::vector<std::string>>::iterator it = (_agenda.find(dia)->second).begin();
 
     std::cout << "Atividades do dia " << dia << ": " << std::endl << std::endl;
@@ -28,7 +35,7 @@ void Calendario::imprimirCalendario(std::string dia) {
         }
 
         for (int i=0; i < (it->second).size(); i++) {
-
+            
             std::cout << (it->second)[i] << std::endl;
         }
         it++;
