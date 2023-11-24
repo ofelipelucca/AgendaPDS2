44b9973ae1fc6fa2009d245a6877f8de0cd7287b
felipeluccaTO <lipelucca100@gmail.com>
2023-11-24 15:03:03 -0300

    Alteraçoes no menu usuario

--- Menu/UsuarioMenu.cpp
+++ Menu/UsuarioMenu.cpp
@@ -11,52 +11,41 @@ UsuarioMenu::UsuarioMenu(std::string* user_nome, std::string* user_email) {
   setSubtitulo("Calendario da semana:");
   setOpcoes({"1 --> Compromissos", "2 --> Lembretes", "3 --> Tarefas"});
   setUsuario(*user_email);
+
+  Calendario user_calendario;
+
+  user_calendario.imprimirCalendario(); ///< Imprimindo o calendario da semana
 }
 
 PrimeiroMenu *UsuarioMenu::next(unsigned option) {
   switch (option) {
-  case 1: {
-    
-    imprimirCalendario();
-    //imprime o calendario da semana 
+    case 1: {
 
-    std::cout << '\n' '\n' << "Aqui estão seus lembretes: " << std::endl;
-    std::cout << '\n' << ListaLembrete::verLembretes(getUsuario()) << std::endl;
-    // imprime os lembretes atuais 
-    
-    ///...
-     
-    return new LembreteMenu();
-    ///vai para o menu dos lembretes (a criar)
-    /// conferir se o parametro é o usuário mesmo
-  }
-  case 2: {
-    imprimirCalendario();
-    //imprime o calendario da semana 
+      std::cout << std::endl << "Aqui estao seus lembretes salvos: " << std::endl;
+      std::cout << std::endl << ListaLembrete::verLembretes(getUsuario()) << std::endl;
+      
+      return new LembreteMenu();
+      ///vai para o menu dos lembretes (a criar)
+      /// conferir se o parametro é o usuário mesmo
+    }
+    case 2: {
 
-    std::cout << '\n' '\n' << "Aqui estão seus Compromissos: " << std::endl;
-    std::cout <<'\n' << verCompromissos() << std::endl;
-    /// imprime os Compromissos atuais 
-    
-    ///...
-     
-    return new CompromissoMenu();
-    ///vai para o menu dos Compromissos (a criar)
-    /// conferir se o parametro é o usuário mesmo
-  }
-  case 3: {
-    imprimirCalendario();
-    //imprime o calendario da semana 
+      std::cout << std::endl << "Aqui estao seus compromissos salvos: " << std::endl;
+      std::cout << std::endl << verCompromissos(getUsuario()) << std::endl;
+      
+      return new CompromissoMenu();
+      ///vai para o menu dos Compromissos (a criar)
+      /// conferir se o parametro é o usuário mesmo
+    }
+    case 3: {
 
-    std::cout << '\n' << "Aqui estão suas Tarefas: " << std::endl;
-    std::cout << '\n' << verTarefas() << std::endl;
-    /// imprime as Tarefas atuais 
-    
-    ///...
-     return new TarefaMenu();
-    ///vai para o menu das Tarefas (a criar) 
-    /// conferir se o parametro é o usuário mesmo
-  }
+      std::cout << std::endl << "Aqui estao suas tarefas salvas: " << std::endl;
+      std::cout << std::endl << verTarefas(getUsuario()) << std::endl;
+
+      return new TarefaMenu();
+      ///vai para o menu das Tarefas (a criar) 
+      /// conferir se o parametro é o usuário mesmo
+    }
   }
 
   // TODO: próximo menu
