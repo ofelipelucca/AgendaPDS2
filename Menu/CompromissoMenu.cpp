b8a3b95222f02fc18574f0f2f45f7309a603ad5c
thiagotvos <150938745+thiagotvos@users.noreply.github.com>
2023-11-28 18:47:13 -0300

    Fazendo codigo CompromissoMenu.cpp

--- Menu/CompromissoMenu.cpp
+++ Menu/CompromissoMenu.cpp
@@ -1,10 +1,43 @@
-#pragma once
-
-#include "UsuarioMenu.hpp"
-#include "LembreteMenu.hpp"
 #include "CompromissoMenu.hpp"
-#include "TarefaMenu.hpp"
-
 #include <iostream>
-#include <string>
 
+// Implementação dos métodos da classe CompromissoMenu
+
+CompromissoMenu::CompromissoMenu(std::string *login_email) {
+    setUsuario(*login_email);
+    setTitulo("Menu de Compromissos");
+    setSubtitulo("Escolha uma opção:");
+    setOpcoes({"1 --> Ver Compromissos", "2 --> Adicionar Compromisso", "3 --> Remover Compromisso", "4 --> Voltar"});
+}
+
+PrimeiroMenu *CompromissoMenu::next(unsigned option) {
+    PrimeiroMenu *proximoMenu = nullptr;
+
+    switch (option) {
+        case 1:
+            // Implementar a visualização dos compromissos
+            // Chame a função para mostrar os compromissos aqui
+            std::cout << "Visualizando Compromissos..." << std::endl;
+            break;
+        case 2:
+            // Implementar a adição de um compromisso
+            // Chame a função para adicionar compromisso aqui
+            std::cout << "Adicionando Compromisso..." << std::endl;
+            break;
+        case 3:
+            // Implementar a remoção de um compromisso
+            // Chame a função para remover compromisso aqui
+            std::cout << "Removendo Compromisso..." << std::endl;
+            break;
+        case 4:
+            // Voltar para o menu anterior (pode ser o Menu Principal ou outro)
+            std::cout << "Voltando..." << std::endl;
+            break;
+        default:
+            std::cout << "Opção inválida!" << std::endl;
+            break;
+    }
+
+    // Retorna o próximo menu, se houver
+    return proximoMenu;
+}
