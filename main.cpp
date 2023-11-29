76b4e2239e49cd2cce5f4271e32e1460f0bda50c
rob1913r <148152111+rob1913r@users.noreply.github.com>
2023-11-29 13:37:34 -0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,32 @@
+#include "Menu.hpp"
+#include "LoginMenu.hpp"
+#include "UsuarioMenu.hpp"
+#include "LembreteMenu.hpp"
+#include "CompromissoMenu.hpp"
+#include "TarefaMenu.hpp"
+
+#include <iostream>
+
+int main() {
+  MenuLogin login;
+  login.renderizar();
+  // renderiza a pagina do menu
+  
+  unsigned escolha;
+  std::cin >> escolha;
+
+  PrimeiroMenu *menu = login.next(escolha, nullptr);
+  //pega a escolha do usuário e atribue a função
+
+  while (menu != nullptr) {
+    menu->renderizar();
+    std::cin >> escolha;
+
+    PrimeiroMenu *old_menu = menu;
+    menu = menu->next(escolha, old_menu);
+
+    delete old_menu;
+  }
+  
+  return 0;
+}
