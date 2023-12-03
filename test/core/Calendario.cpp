f3393ee63e4d23dbfb7353628c4a426fc29b4269
Roberto Campolina <robertojuniorme@gmail.com>
2023-12-03 18:11:18 -0300

    Commit

--- test/core/Calendario.cpp
+++ test/core/Calendario.cpp
@@ -8,22 +8,22 @@ TEST_CASE("Mapeando atividades para o calendário") {
         c.inserir("10:00:00", {"Estudar para a prova"});
         c.inserir("15:00:00", {"Ir para faculdade", "Terminar o trabalho"});
 
-        CHECK_EQ(c.size_mapaHorario(), 2);
+        CHECK_EQ(c.sizeMapaHorario(), 2);
 
         c.inserir("26/11/2023");
 
-        CHECK_EQ(c.size_mapaHorario(), 0);
+        CHECK_EQ(c.sizeMapaHorario(), 0);
     }
 
     SUBCASE("Mapeando horários para um dia") {
         c.inserir("10:00:00", {"Estudar para a prova"});
 
         c.inserir("26/11/2023");
-        CHECK_EQ(c.size_agenda(), 1);
+        CHECK_EQ(c.sizeAgenda(), 1);
 
         c.inserir("15:00:00", {"Ir para faculdade", "Terminar o trabalho"});
 
         c.inserir("03/12/2023");
-        CHECK_EQ(c.size_agenda(), 2);
+        CHECK_EQ(c.sizeAgenda(), 2);
     }
 }
